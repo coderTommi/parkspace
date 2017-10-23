2b732bf3680be2b2f234d8211be0dd8e175f6ad4
codertommi <codertommy@sina.com>
2017-10-23 19:07:00 +0800

    添加controller接口

---
+++ src/test/java/test/baseuser/dao/TestClass.java
@@ -0,0 +1,26 @@
+package test.baseuser.dao;
+/**
+ * @Title: TestClass.java
+ * @Package test.baseuser.dao
+ * <p>Description:</p>
+ * @author lidongliang
+ * @version V1.0.0 
+ * <p>CreateDate:2017年10月20日 下午3:47:33</p>
+*/
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		{
+			int x=3;
+			System.out.println("1, x="+x);
+		}
+		int x=1;
+		System.out.println("2, x="+x);
+		{
+			int y=2;
+			System.out.println("3, y="+y);
+		}
+	}
+
+}
